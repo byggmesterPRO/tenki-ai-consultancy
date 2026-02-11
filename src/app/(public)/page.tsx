5b8fa79b56dc0111978c588c36f58072cb0a8e53
Einar <einar.k.holt@gmail.com>
2026-02-11 01:49:28 +0100

    fix: force dynamic rendering on homepage to avoid prerender errors

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,8 @@
 import Home from "@/components/pages/Home";
 import BlogPreview from "@/components/BlogPreview";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   return <Home blogPreview={<BlogPreview />} />;
 }
