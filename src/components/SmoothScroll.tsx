efb3d770905afd087864fba6ffdbe57f0de5def2
Einar <einar.k.holt@gmail.com>
2026-02-12 01:26:01 +0100

    fix: Restore Lenis smooth scroll

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,5 +1,33 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    let lenis: import("lenis").default | null = null;
+    let rafId: number;
+
+    import("lenis").then((mod) => {
+      lenis = new mod.default({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        touchMultiplier: 2,
+        infinite: false,
+      });
+
+      function raf(time: number) {
+        lenis?.raf(time);
+        rafId = requestAnimationFrame(raf);
+      }
+
+      rafId = requestAnimationFrame(raf);
+    });
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis?.destroy();
+    };
+  }, []);
+
   return <>{children}</>;
 }
